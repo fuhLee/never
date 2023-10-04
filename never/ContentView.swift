2b5a916d442ba33e3eec394ec62c6844f729b223
kevinwforney <kevinwilliamforney@gmail.com>
2023-10-04 10:41:30 +0800

    Initial Commit

---
+++ never/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  never
+//
+//  Created by User03 on 2023/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ never/neverApp.swift
@@ -0,0 +1,17 @@
+//
+//  neverApp.swift
+//  never
+//
+//  Created by User03 on 2023/10/4.
+//
+
+import SwiftUI
+
+@main
+struct neverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
